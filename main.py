de5c7f64c2f547c7541bd34d995b19c549a96828
hunaidkhan <hunaid20042004@gmail.com>
2021-08-07 15:08:24 -0600

    changed project structure

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask.json import jsonify
 from flask_restful import Api, Resource
 from youtube_transcript_api import YouTubeTranscriptApi
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +10,10 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self, videoId):
         srt = YouTubeTranscriptApi.get_transcript(videoId,languages=['en'])
+        out_file = open("data/subtitles.json", "w")
+        out_file.truncate(0)
+        json.dump(srt, out_file, indent = 4, sort_keys = False)
+        out_file.close()
         return jsonify(srt)
 
     # def post(self, videoId):
